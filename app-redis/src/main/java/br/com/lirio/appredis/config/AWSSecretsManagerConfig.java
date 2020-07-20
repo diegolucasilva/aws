a2207bfcde75f3cfb2915ab76b894855f788ca39
Diego Lucas <diego.lucasilva@gmail.com>
2020-07-20 15:23:54 -0300

    only 1 config class

--- app-redis/src/main/java/br/com/lirio/appredis/config/AWSSecretsManagerConfig.java
+++ app-redis/src/main/java/br/com/lirio/appredis/config/AWSSecretsManagerConfig.java
@@ -1,39 +1,53 @@
 package br.com.lirio.appredis.config;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-@Profile(value = {"develop"})
 @Configuration
+@Slf4j
 public class AWSSecretsManagerConfig {
 
-    @Value("${aws.secretsManager.secretName}")
-    private String secretName;
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurerDev(ConfigurableEnvironment env) throws JsonProcessingException {
 
-    @Value("${aws.secretsManager.region}")
-    private String region;
+        final String secretName = env.getProperty("aws.secretsManager.secretName");
+        final String region = env.getProperty("aws.secretsManager.region");
+        final String endpoint = env.getProperty("aws.secretsManager.endpoint");
 
-    @Bean("awsSecretRedisAuth")
-    public String getSecret() throws JsonProcessingException {
-        //     AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
-        //                                                              .withRegion(region)
-        //                                                              .build();
+        AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .build();
 
-        //     GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
-        //     GetSecretValueResult getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
+        GetSecretValueResult getSecretValueResult = client.getSecretValue(getSecretValueRequest);
 
-        //     String secret = getSecretValueResult.getSecretString();
-        //     RedisAuth redisAuth = new ObjectMapper().readValue(secret, RedisAuth.class);
-        //     return redisAuth.getRedisAuth();
-        return "d72f3d09-a29c-4996-8419-486c5d4528d5";
+        String secret = getSecretValueResult.getSecretString();
+        RedisAuth redisAuth = new ObjectMapper().readValue(secret, RedisAuth.class);
+        log.info("Redis secret  => {}", redisAuth.getRedisAuth());
+
+        MutablePropertySources propertySources = env.getPropertySources();
+        Map<String, Object> map = new HashMap<>();
+        map.put("spring.redis.password",
+                redisAuth.getRedisAuth());
+        propertySources
+                .addFirst(new MapPropertySource("newmap", map));
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
